562fa0982244e725661be1f8a0d60b411a134ba2
Kelok <kelokchan@gmail.com>
2016-02-09 00:36:57 +0800

    renamed

--- src/atmattempt/Account.java
+++ src/atmattempt/Account.java
@@ -1,9 +1,11 @@
+package atmattempt;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atmattempt;
+
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
